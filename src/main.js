8af74272c624fdc8e2b31d26d3039a52ef487ffa
moddi3 <vlad211099@gmail.com>
2017-02-07 00:19:48 +0200

    deleted random catalog😀

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-console.log(15+35);
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
-// document.getElementById('app').innerHTML = 'Привет, я готов. Как дела?'
-module.hot.accept();
