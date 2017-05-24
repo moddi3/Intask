9d15d96f48f841ff70e601cd9476be6987647760
Vlad Ivanov <vlad211099@gmail.com>
2017-05-24 21:14:18 +0300

    added friendly-errors plugin

---
+++ webpack.config.babel.js
@@ -0,0 +1,90 @@
+import path from 'path';
+import webpack from 'webpack';
+import FriendlyErrorsPlugin from 'friendly-errors-webpack-plugin';
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    'babel-polyfill',
+    './client/main',
+  ],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'build.js',
+    publicPath: '/dist/',
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new FriendlyErrorsPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            scss: 'vue-style-loader!css-loader!sass-loader',
+            sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+          },
+          // other vue-loader options go here
+        },
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/,
+        options: {
+          emitWarning: true,
+        },
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]',
+        },
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.common.js',
+      '@': path.join(__dirname, '..', 'src'),
+    },
+  },
+  performance: {
+    hints: false,
+  },
+};
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map';
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || [])
+    .concat([
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: '"production"',
+        },
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true,
+        compress: {
+          warnings: false,
+        },
+      }),
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+      }),
+    ]);
+}
