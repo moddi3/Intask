54c0f2ab866d52fd13e0fc19b2d731583a40654c
Vlad Ivanov <vlad211099@gmail.com>
2017-05-31 12:14:57 +0300

    removed unused routes

--- server/routes/app.js
+++ server/routes/app.js
@@ -5,10 +5,4 @@ const router = Router();
 
 router.get('/', todo.all);
 
-// router.post('/', todo.create);
-
-// router.put('/:slug', todo.update);
-
-// router.delete('/:slug', todo.delete);
-
 export default router;
