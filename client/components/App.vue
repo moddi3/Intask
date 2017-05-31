4aa543163b2ace0f04f4eed4e8031bd54e4a8a9b
Vlad Ivanov <vlad211099@gmail.com>
2017-05-31 12:21:44 +0300

    styles now in a separate file

--- client/components/App.vue
+++ client/components/App.vue
@@ -1,37 +1,23 @@
 <template>
   <div id="app">
-    <img src="../assets/logo.png">
-    <todo-list></todo-list>
+    <header>
+      <span class="logo">Inta<span class="logo-accent">s</span>k</span>
+    </header>
+    <div class="main">
+      <todo-list ref="list" />
+    </div>
   </div>
 </template>
 
 <script>
-  import TodoList from './TodoList.vue';
+import TodoList from './TodoList.vue';
 
-  export default {
-    name: 'app',
-    components: {
-      TodoList,
-    },
-  };
+export default {
+  name: 'app',
+  components: {
+    TodoList,
+  },
+};
 </script>
 
-<style>
-html,body { height: 100%; }
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-    height: 100%;
-    width: 100%;
-    margin: 0 auto;
-  }
-  
-  h1,
-  h2 {
-    font-weight: normal;
-  }
-</style>
+<style src="../assets/css/main.css"></style>
